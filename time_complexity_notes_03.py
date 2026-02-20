ea909bb20b446d711069267f4d4968d798906159
kimiazarei <kimiazarei1385km@gmail.com>
2026-02-20 13:48:07 +0330

    Update time_complexity_notes_03.py

--- time_complexity_notes_03.py
+++ time_complexity_notes_03.py
@@ -1,9 +1,10 @@
-"""Time complexity notes (refactored from original exercise_3.py)
+"""Time complexity notes
 
 "تابع پیجیدگی زمانی تکه برنامه زیر را بنویسید"
 for i in range(0,n+3,3):
     t=t+5 
     l=2*l 
+
 
     F(n) = 2/3(n+3) = 0.6 n + 2
 """
