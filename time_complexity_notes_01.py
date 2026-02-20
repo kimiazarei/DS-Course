309f144d3042158e58ebdfaec61d11563619dbb6
kimiazarei <kimiazarei1385km@gmail.com>
2026-02-20 13:47:27 +0330

    Update time_complexity_notes_01.py

--- time_complexity_notes_01.py
+++ time_complexity_notes_01.py
@@ -1,4 +1,4 @@
-"""Time complexity notes (refactored from original exercise_1.py)
+"""Time complexity notes
 
 "تابع پیچیدگی زمانی الگوریتم زیر را محاسبه کنید"
 a = 5
@@ -6,6 +6,7 @@ for i in range (n):
     p=t
     t=l+6
 l = 2 + l
+
 
 F(n) = 2 n + 2
 """
